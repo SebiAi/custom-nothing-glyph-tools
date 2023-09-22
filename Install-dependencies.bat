788f7b10c2a1016477b0dbc48f527a675f37725e
Yann <yannluca@naeher.eu>
2023-09-22 17:53:08 +0200

    Update Install-dependencies.bat
    added automatic elevation
    added automatic winget install
    added hash checker for automatic winget install
    added some comments

--- Install-dependencies.bat
+++ Install-dependencies.bat
@@ -1,11 +1,70 @@
 @echo off
-:: Maybe add a Windows Version check here?
+REM Maybe add a Windows Version check here?
 
+:: ---------------------------check for admin rights-----------------------------------------------------
+@(
+    (
+        net session >nul 2>&1
+    ) || (
+        :: elevate the script to admin
+        powershell Start-Process -FilePath '%0' -Verb RunAs
+        goto :EOF
+    )
+)
+
+:: ---------------------------check for winget-----------------------------------------------------------
 :tryWinget
 winget
 if %errorlevel%==0 goto :install
 cls
+:: ---------------------------automated WinGet install--------------------------------------------------
+setlocal
+:: check if the folder ".tmp" exists and create it if it doesn't
+if exist "%~dp0/.tmp" (
+  echo The folder ".tmp" exists.
+) else (
+  mkdir "%~dp0/.tmp"
+  attrib +h "%~dp0/.tmp" /s /d
+)
+cls
+echo Downloading WinGet
+
+:: Download the latest version of WinGet and save it to the .tmp folder as WinGet.msixbundle
+powershell -Command "Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "%~dp0/.tmp/WinGet.msixbundle""
+
+:: get hash of WinGet.msixbundle and save it to the .tmp folder as file_hash.txt
+CertUtil -hashfile "%~dp0/.tmp/WinGet.msixbundle" SHA256 > %~dp0/.tmp/file_hash.txt
+
+:: get hash of the latest version of WinGet from web and save it to the .tmp folder as hash.txt
+powershell -Command "Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.txt" -OutFile "%~dp0/.tmp/hash.txt""
+
+:: get the hash value from the hash.txt file
+(for /L %%i in (1,1,1) do set /P "hash=") < "%~dp0/.tmp/hash.txt"
+echo The file hash is: %hash%
+
+:: get the hash value from the file_hash.txt file
+(for /L %%i in (1,1,2) do set /P "file_hash=") < "%~dp0/.tmp/file_hash.txt"
+echo The file hash is: %file_hash%
+
+:: Compare the hash values
+if "%file_hash%"=="%hash%" (
+    echo The hashes match!
+) else (
+    echo The hashes do not match.
+    goto :manualInstall
+)
+
+endlocal
+
+:: Install WinGet
+powershell -Command "Add-AppxPackage "%~dp0/.tmp/WinGet.msixbundle"
+:: if the installation doesn't fail, try if winget works now
+if %errorlevel%==0 goto :tryWinget
+
+:: if the installation fails, try to install it from the Microsoft Store manually
+:manualInstall
 echo Please install/update AppInstaller from the Microsoft Store.
+::open the Microsoft Store page for AppInstaller
 start ms-windows-store://pdp/?ProductId=9nblggh4nns1
 echo.
 pause
@@ -13,40 +72,50 @@ goto :tryWinget
 
 :install
 cls
+:: ask the user if they want to install Audacity or not which is Optional
 set /p "install=Do you want to install Audacity? (y/n): "
 if /i "%install%"=="y" goto :fullInstall
 if /i "%install%"=="n" goto :basicInstall
 else goto :install
 
 :fullInstall
+:: install Audacity
 winget install Audacity.Audacity
 echo.
 call :basicInstall
 goto :restartScript
 
 :basicInstall
+:: install the rest of the programs
 winget install Gyan.FFmpeg
 echo.
 winget install Python.Python.3.11
 echo.
 
+:: try if python command is working
 :restartScript
 python --version
+:: if the python command is working, go to the next step
 if %errorlevel%==0 goto :installPythonStuff
 cls
+:: if the python command is not working, try to refresh the environment
 goto :refrenv
 pause
 
 :installPythonStuff
 cls
+:: install the python packages
 pip install -r requirements.txt
 echo.
 
 :end
 powershell write-host -fore Green "Success!"
 pause
+:: delete the .tmp folder and exit
+rd /S /Q "%~dp0/.tmp"
 exit
-
+:: ----------------------------------------------------------------------------------------------------
+:: refresh environment variables with the RefrEnv script
 
 REM refrenv.bat script by @badrelmers https://github.com/badrelmers
 REM Source: https://github.com/badrelmers/RefrEnv
